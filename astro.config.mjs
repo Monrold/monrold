c04329de1033008447d905fcde033b65cecd683b
Monrold <soporte.draw@gmail.com>
2025-07-27 21:26:18 -0600

    feat: tailwind version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   integrations: [  // solo la integración oficial de Astro para Tailwind
     react(),
   ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
