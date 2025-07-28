52bc1ec2500aa3fd972744442db7718e3ad930b7
Monrold <soporte.draw@gmail.com>
2025-07-27 19:44:46 -0600

    feat: tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 
 import tailwind from "@astrojs/tailwind";
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -12,5 +14,9 @@ export default defineConfig({
     react({
       include: ['**/react/*'],
     }),
-  ]
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
