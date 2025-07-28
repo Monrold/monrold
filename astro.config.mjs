5bb3e9330a05391a46a0c1b6ad3e3869b8aee4a4
Monrold <soporte.draw@gmail.com>
2025-07-27 20:04:05 -0600

    feat: tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
-
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind({
-      include: ['**/tailwind/*'],
-    }),
-    react({
-      include: ['**/react/*'],
-    }),
+    tailwind(),  // usa la integración sin opciones a menos que tengas una razón específica
+    react(),
   ],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  // No agregues el plugin tailwindcss manualmente en vite.plugins
 });
